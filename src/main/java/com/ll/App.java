a8743688eb9059701d227c369bd4018f5ec70818
schim613 <uximin@naver.com>
2023-10-25 16:37:35 +0900

    1단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,26 @@
 package com.ll;
 
+// System.out 모니터
+// System.in  키보드
+
+import java.util.Scanner;
+
 class App {
     void run() {
-        System.out.println("프로그램 실행");
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("종료")) {
+                System.out.println("== 종료되었습니다. ==");
+                break;
+            }
+
+            System.out.printf("입력받은 값 : %s\n", cmd);
+        }
     }
 }
