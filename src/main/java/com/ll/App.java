03b52f8b2115732985ccd31a6539f01d43dc9ef3
schim613 <uximin@naver.com>
2023-10-29 22:16:14 +0900

    step 2, input

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -3,24 +3,36 @@ package com.ll;
 // System.out 모니터
 // System.in  키보드
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class App {
+    Scanner scanner = new Scanner(System.in);
+    int lastContentId = 0;
+
     void run() {
         System.out.println("== 명언 앱 ==");
 
         while (true) {
             System.out.print("명령) ");
 
-            Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
+            if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String quotation = scanner.nextLine();
 
-            if (cmd.equals("종료")) {
-                System.out.println("== 종료되었습니다. ==");
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
+
+                lastContentId++;
+                System.out.printf("명언 : %s, 작가 : %s\n", quotation, authorName);
+            }
+            else if (cmd.equals("종료")) {
                 break;
             }
-
-            System.out.printf("입력받은 값 : %s\n", cmd);
+            else if (cmd.equals("목록")) {
+            }
         }
     }
 }
