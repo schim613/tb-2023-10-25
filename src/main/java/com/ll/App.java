803ee6e5680cbd66fe8d67f13c56564d55fd20af
schim613 <uximin@naver.com>
2023-10-29 23:46:09 +0900

    step 5, print list

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -39,7 +39,20 @@ class App {
                 break;
             }
             else if (cmd.equals("목록")) {
-                System.out.println("총 개수 : " + quotations.size());
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------");
+
+                if (quotations.isEmpty())
+                    System.out.println("등록된 명언이 없습니다.");
+
+                for (int i = quotations.size() - 1; i >= 0; i--) {
+                    Quotation quotation = quotations.get(i);
+                    System.out.printf("%d / %s / %s\n", quotation.id, quotation.content, quotation.authorName);
+                }
+
+//                for (Quotation quotation : quotations) {
+//                    System.out.printf("%d / %s / %s\n", quotation.id, quotation.content, quotation.authorName);
+//                }
             }
         }
     }
