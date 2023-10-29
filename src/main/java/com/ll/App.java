3cdaabfc68c08272c1b114d2942fee886c92a608
schim613 <uximin@naver.com>
2023-10-29 23:18:48 +0900

    step 5, quotations.add()

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 
 class App {
     Scanner scanner = new Scanner(System.in);
+    List<Quotation> quotations = new ArrayList<>();
 
     void run() {
         System.out.println("== 명언 앱 ==");
@@ -20,20 +21,25 @@ class App {
             String cmd = scanner.nextLine();
             if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String quotation = scanner.nextLine();
+                String content = scanner.nextLine();
 
                 System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
 
                 lastQuotationtId++;
+                int id = lastQuotationtId;
+
+                Quotation quotation = new Quotation(lastQuotationtId, content, authorName);
+                quotations.add(quotation);
 
                 System.out.printf("%d번 명언이 등록되었습니다\n", lastQuotationtId);
-                System.out.printf("명언 : %s, 작가 : %s\n", quotation, authorName);
+                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
             }
             else if (cmd.equals("종료")) {
                 break;
             }
             else if (cmd.equals("목록")) {
+                System.out.println("총 개수 : " + quotations.size());
             }
         }
     }
